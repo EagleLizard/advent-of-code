0531500adbd9c039a9237408bfcd8b55964e788e
eaglelizard <whitedtd@gmail.com>
2023-12-08 17:55:24 -0700

    Day 3 part 2 typescript solution

--- 2023/src/day3/day3.ts
+++ 2023/src/day3/day3.ts
@@ -10,6 +10,12 @@ const DAY3_INPUT_FILE_PATH = [
   DAY3_INPUT_FILE_NAME,
 ].join(path.sep);
 
+type PartNum = {
+  val: string;
+  x: number;
+  y: number;
+};
+
 export async function day3Main() {
   console.log('~ Day 3 ~');
   let inputStrs: string[];
@@ -18,6 +24,126 @@ export async function day3Main() {
   // reference input as 2d matrics
   grid = inputStrs.filter(inputStr => inputStr.length > 1);
 
+  day1Part1(grid);
+  day1Part2(grid);
+}
+
+function day1Part2(grid: string[]) {
+  console.log('~ Day 1 part 2 ~');
+  /*
+    A gear is any * symbol that is adjacent to exactly two part numbers.
+    Its gear ratio is the result of multiplying those two numbers together.
+  */
+  /*
+    Do 2 passes
+    Pass 1:
+      Extract all of the numbers adjacent to gears, store the
+        number's value and starting position.
+      Extract all of the gears
+  */
+  let numFlag: boolean;
+  let partFlag: boolean;
+  let numArr: string[];
+  let foundNums: PartNum[];
+  let charX: number;
+  let charY: number;
+  numFlag = false;
+  partFlag = false;
+  numArr = [];
+  foundNums = [];
+
+  charX = -1;
+  charY = -1;
+
+  const terminateNum = () => {
+    numFlag = false;
+    if(partFlag) {
+      partFlag = false;
+      foundNums.push({
+        val: numArr.join(''),
+        x: charX,
+        y: charY,
+      });
+    }
+    numArr = [];
+    charX = -1;
+    charY = -1;
+  };
+
+  /*
+    Find every number that is adjacent to a gear
+  */
+  for(let y = 0; y < grid.length; ++y) {
+    let currRow: string;
+    currRow = grid[y];
+    for(let x = 0; x < currRow.length; ++x) {
+      let char: string;
+      char = currRow[x];
+      if(!numFlag && isNumChar(char)) {
+        numFlag = true;
+        charX = x;
+        charY = y;
+      }
+      if(numFlag) {
+        if(!isNumChar(char)) {
+          terminateNum();
+        } else {
+          if(!partFlag) {
+            if(hasAdjacentSymbol(x, y, grid, /[*]/)) {
+              partFlag = true;
+            }
+          }
+          numArr.push(char);
+        }
+      }
+    }
+    terminateNum();
+    if(numFlag) {
+      // error - shouldn't happen
+      console.log('_' + numArr.join(''));
+    }
+    if(partFlag) {
+      // error - shouldn't happen
+      console.log(numArr.join(''));
+    }
+  }
+
+  /*
+    Find every gear that is adjacent to 2 nums
+  */
+  let gearNums: PartNum[];
+  let gearPairs: [PartNum, PartNum][];
+  gearPairs = [];
+  for(let y = 0; y < grid.length; ++y) {
+    let currRow: string;
+    currRow = grid[y];
+    for(let x = 0; x < currRow.length; ++x) {
+      if(currRow[x] === '*') {
+        // find all PartNums adjacent to this gear
+        gearNums = foundNums.filter(gearNum => {
+          return (y <= gearNum.y + 1)
+            && (y >= gearNum.y - 1)
+            && (x >= gearNum.x - 1)
+            && (x <= (gearNum.x + gearNum.val.length))
+          ;
+        });
+        if(gearNums.length === 2) {
+          gearPairs.push([ gearNums[0], gearNums[1] ]);
+        }
+      }
+    }
+  }
+
+  let gearRatioSum: number;
+  gearRatioSum = gearPairs.reduce((acc, curr) => {
+    return acc + (+curr[0].val * +curr[1].val);
+  }, 0);
+  console.log('Gear ratio sum:');
+  console.log(gearRatioSum);
+}
+
+function day1Part1(grid: string[]) {
+  console.log('~ Day 1 part 1 ~');
   /*
     traverse the grid.
     When encountering a number:
@@ -89,15 +215,15 @@ export async function day3Main() {
 
   console.log('Engine parts sum:');
   console.log(partsSum);
-
 }
 
-function hasAdjacentSymbol(x: number, y: number, grid: string[]): boolean {
+function hasAdjacentSymbol(x: number, y: number, grid: string[], charRx?: RegExp): boolean {
   /*
     Symbols:
       *&@/+#$%=-
   */
   let adjacentChars: string[];
+  let rx = charRx ?? /[*&@/+#$%=-]/;
   adjacentChars = [
     grid[y - 1]?.[x - 1],
     grid[y - 1]?.[x],
@@ -108,7 +234,7 @@ function hasAdjacentSymbol(x: number, y: number, grid: string[]): boolean {
     grid[y + 1]?.[x],
     grid[y + 1]?.[x + 1],
   ].filter(char => char !== undefined);
-  return adjacentChars.some(char => /[*&@/+#$%=-]/.test(char));
+  return adjacentChars.some(char => rx.test(char));
 }
 
 function isNumChar(char: string): boolean {
