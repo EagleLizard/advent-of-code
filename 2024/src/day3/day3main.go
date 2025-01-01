dee100810492634cf8794de28e7589435287ca82
eaglelizard <whitedtd@gmail.com>
2025-01-01 11:07:56 -0700

    2024 day 3 part 2 [go] start parsing do/don't instructions

--- 2024/src/day3/day3main.go
+++ 2024/src/day3/day3main.go
@@ -1,6 +1,7 @@
 package day3
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 )
@@ -17,6 +18,35 @@ func Day3Pt1(inputLines []string) int {
 		mulSum += mulInstruction.Lhs * mulInstruction.Rhs
 	}
 	return mulSum
+}
+
+func Day3Pt2(inputLines []string) int {
+	// rx := regexp.MustCompile(`(do\(\))|(don't\(\))|mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
+	// rx := regexp.MustCompile(`(do\(\)|don't\(\))`)
+	rx := regexp.MustCompile(`(do\(\)|don't\(\))|mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
+	for _, inputLine := range inputLines {
+		lineLen := len(inputLine)
+		for cursorPos := 0; cursorPos < lineLen; {
+			match := rx.FindStringSubmatchIndex(inputLine[cursorPos:])
+			if match == nil {
+				// no more matches
+				break
+			}
+			var fullMatchStart int
+			var fullMatchEnd int
+			if match[0] != -1 {
+				fullMatchStart = cursorPos + match[0]
+			}
+			if match[1] != -1 {
+				fullMatchEnd = cursorPos + match[1]
+			}
+			fullMatchStr := inputLine[fullMatchStart:fullMatchEnd]
+			cursorPos += match[1]
+			fmt.Printf("%v\n", match)
+			fmt.Printf("%s\n", fullMatchStr)
+		}
+	}
+	return -1
 }
 
 func parseInput(inputLines []string) []MulInstruction {
