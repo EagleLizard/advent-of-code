e3ff08ec3574ff91b9064cb8575e11e1a55c0174
eaglelizard <whitedtd@gmail.com>
2025-01-02 12:23:03 -0700

    day 4 part 1 [go] attempt 1, wrong answer

--- 2024/src/day4/day4main.go
+++ 2024/src/day4/day4main.go
@@ -9,6 +9,10 @@ type Point struct {
 	y int
 }
 
+/*
+2310 - too low
+*/
+
 func Day4Pt1(inputLines []string) int {
 	searchChars := []rune{'X', 'M', 'A', 'S'}
 	searchStrLen := len(searchChars)
@@ -19,6 +23,13 @@ func Day4Pt1(inputLines []string) int {
 	downMatches := []Point{}
 	backMatches := []Point{}
 	upMatches := []Point{}
+	/* diagonal */
+	seMatches := []Point{}
+	neMatches := []Point{}
+	swMatches := []Point{}
+	nwMatches := []Point{}
+
+	matchCount := 0
 	// fmt.Printf("%q\n", parsedLines)
 	for y, parsedLine := range parsedLines {
 		lineLen := len(parsedLine)
@@ -27,13 +38,23 @@ func Day4Pt1(inputLines []string) int {
 			var downMatch bool
 			var backMatch bool
 			var upMatch bool
+			/* diagonal */
+			var seMatch bool
+			var neMatch bool
+			var swMatch bool
+			var nwMatch bool
+			// if currChar == 'X' {
+			// 	fmt.Print("X")
+			// } else {
+			// 	fmt.Print(".")
+			// }
 			if currChar != searchChars[0] {
 				continue
 			}
 			lookForward := x+searchStrLen < lineLen
 			lookDown := y+searchStrLen < numLines
-			lookLeft := x-searchStrLen > 0
-			lookUp := y-searchStrLen > 0
+			lookLeft := x-(searchStrLen-1) >= 0
+			lookUp := y-(searchStrLen-1) >= 0
 			// fmt.Printf("%q,", currChar)
 			if lookForward {
 				/* look forward */
@@ -42,6 +63,7 @@ func Day4Pt1(inputLines []string) int {
 					forwardMatch = parsedLine[x+i] == searchChars[i]
 				}
 				if forwardMatch {
+					matchCount++
 					forwardMatches = append(forwardMatches, Point{x, y})
 				}
 			}
@@ -52,7 +74,7 @@ func Day4Pt1(inputLines []string) int {
 					downMatch = parsedLines[y+i][x] == searchChars[i]
 				}
 				if downMatch {
-					fmt.Printf("%q\n", parsedLine)
+					matchCount++
 					downMatches = append(downMatches, Point{x, y})
 				}
 			}
@@ -63,6 +85,7 @@ func Day4Pt1(inputLines []string) int {
 					backMatch = parsedLine[x-i] == searchChars[i]
 				}
 				if backMatch {
+					matchCount++
 					backMatches = append(backMatches, Point{x, y})
 				}
 			}
@@ -73,30 +96,75 @@ func Day4Pt1(inputLines []string) int {
 					upMatch = parsedLines[y-i][x] == searchChars[i]
 				}
 				if upMatch {
+					matchCount++
 					upMatches = append(upMatches, Point{x, y})
 				}
 			}
 
 			if lookForward && lookDown {
 				/* look SE */
+				seMatch = true
+				for i := 1; seMatch && i < searchStrLen; i++ {
+					seMatch = parsedLines[y+i][x+i] == searchChars[i]
+				}
+				if seMatch {
+					matchCount++
+					seMatches = append(seMatches, Point{x, y})
+				}
 			}
 			if lookForward && lookUp {
 				/* look NE */
+				neMatch = true
+				for i := 1; neMatch && i < searchStrLen; i++ {
+					neMatch = parsedLines[y-i][x+i] == searchChars[i]
+				}
+				if neMatch {
+					matchCount++
+					neMatches = append(neMatches, Point{x, y})
+				}
 			}
 			if lookLeft && lookDown {
 				/* look SW */
+				swMatch = true
+				for i := 1; swMatch && i < searchStrLen; i++ {
+					swMatch = parsedLines[y+i][x-i] == searchChars[i]
+				}
+				if swMatch {
+					matchCount++
+					swMatches = append(swMatches, Point{x, y})
+				}
 			}
 			if lookLeft && lookUp {
 				/* look NW */
+				nwMatch = true
+				for i := 1; nwMatch && i < searchStrLen; i++ {
+					// if x == 4 && y == 9 {
+					// 	fmt.Printf("%v\n", parsedLines[y-i][x-i])
+					// }
+					nwMatch = parsedLines[y-i][x-i] == searchChars[i]
+				}
+				if nwMatch {
+					matchCount++
+					nwMatches = append(nwMatches, Point{x, y})
+				}
 			}
+			// if x == 3 && y == 9 {
+			// 	fmt.Printf("up: %v,\n down: %v,\n left: %v,\n right: %v,\n ", lookUp, lookDown, lookLeft, lookForward)
+			// }
 		}
 		// fmt.Print("\n")
 	}
-	fmt.Printf("fwd matches:\n%v\n", forwardMatches)
-	fmt.Printf("down matches:\n%v\n", downMatches)
-	fmt.Printf("back matches:\n%v\n", backMatches)
-	fmt.Printf("up matches:\n%v\n", upMatches)
-	return -1
+	// fmt.Printf("fwd matches:\n%v\n", forwardMatches)
+	// fmt.Printf("down matches:\n%v\n", downMatches)
+	// fmt.Printf("back matches:\n%v\n", backMatches)
+	// fmt.Printf("up matches:\n%v\n", upMatches)
+	// /* diagonal */
+	// fmt.Printf("SE matches:\n%v\n", seMatches)
+	// fmt.Printf("NE matches:\n%v\n", neMatches)
+	// fmt.Printf("SW matches:\n%v\n", swMatches)
+	// fmt.Printf("NW matches:\n%v\n", nwMatches)
+
+	return matchCount
 }
 
 func parseInput(inputLines []string) [][]rune {
