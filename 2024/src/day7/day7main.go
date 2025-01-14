588783a80bb0fead7368b2497806f2ce89ce7dc7
eaglelizard <whitedtd@gmail.com>
2025-01-14 08:51:10 -0700

    2024 d7p1 [go] solution

--- 2024/src/day7/day7main.go
+++ 2024/src/day7/day7main.go
@@ -11,16 +11,55 @@ type Day7Input struct {
 	Nums    []int
 }
 
+/*
+5540634308465 - too high
+5540634308362 - correct
+*/
 func Day7Pt1(inputLines []string) int {
 	day7Inputs := parseInput(inputLines)
+	calibrationResult := 0
 	for _, currInput := range day7Inputs {
-		fmt.Printf("%d: ", currInput.TestVal)
-		for _, currNum := range currInput.Nums {
-			fmt.Printf("%d ", currNum)
+		// fmt.Printf("%d: ", currInput.TestVal)
+		// for _, currNum := range currInput.Nums {
+		// 	fmt.Printf("%d ", currNum)
+		// }
+		// fmt.Print("\n")
+		ops := []string{"+", "*"}
+		validEq := checkEquation(currInput.TestVal, currInput.Nums[1:], ops, currInput.Nums[0])
+		if validEq {
+			calibrationResult += currInput.TestVal
 		}
-		fmt.Print("\n")
+		// fmt.Printf("%v\n", validEq)
 	}
-	return -1
+	return calibrationResult
+}
+
+func checkEquation(testVal int, nums []int, ops []string, res int) bool {
+	if len(nums) == 0 {
+		return res == testVal
+	}
+	/*
+		try different combinations
+		1 2 3 4
+	*/
+	for _, op := range ops {
+		currRes := doOp(op, res, nums[0])
+		isValid := checkEquation(testVal, nums[1:], ops, currRes)
+		if isValid {
+			return true
+		}
+	}
+	return false
+}
+
+func doOp(op string, a int, b int) int {
+	switch op {
+	case "+":
+		return a + b
+	case "*":
+		return a * b
+	}
+	panic(fmt.Sprintf("invalid op: %s", op))
 }
 
 func parseInput(inputLines []string) []Day7Input {
