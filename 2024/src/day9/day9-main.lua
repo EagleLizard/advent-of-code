adf7518956ba8eab0a7eecbef348a04e8d440b7e
eaglelizard <whitedtd@gmail.com>
2025-01-16 22:03:57 -0700

    2024 d9p1 [lua] start pt 1

---
+++ 2024/src/day9/day9-main.lua
@@ -0,0 +1,14 @@
+
+local printf = require"../util/printf"
+
+local function day9Pt1(inputLines)
+  local inputLine = inputLines[1]
+  printf("%s\n", inputLine)
+  return -1
+end
+
+local day9MainModule = {
+  day9Pt1 = day9Pt1,
+}
+
+return day9MainModule
