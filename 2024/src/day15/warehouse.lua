79a3c04db6608af6c0ced0e5ab111b67242fd7de
eaglelizard <whitedtd@gmail.com>
2025-01-29 16:43:24 -0700

    add warehouse classes into own module

---
+++ 2024/src/day15/warehouse.lua
@@ -0,0 +1,128 @@
+
+local Point = require("geom.point")
+
+local printf = require("util.printf")
+
+local Box = (function ()
+  ---@class Box
+  ---@field origin Point
+  ---@field x integer
+  ---@field y integer
+  local Box = {}
+  Box.__index = Box
+
+  ---@param x integer
+  ---@param y integer
+  function Box.new(x, y)
+    local self = setmetatable({}, Box)
+    self.origin = Point.new(x, y)
+    self.x = x
+    self.y = y
+    return self
+  end
+
+  return Box
+end)()
+
+local Robot = (function ()
+  ---@class Robot
+  ---@field origin Point
+  ---@field x integer
+  ---@field y integer
+  local Robot = {}
+  Robot.__index = Robot
+
+  ---@param x integer
+  ---@param y integer
+  function Robot.new(x, y)
+    local self = setmetatable({}, Robot)
+    self.origin = Point.new(x, y)
+    self.x = x
+    self.y = y
+    return self
+  end
+
+  return Robot
+end)()
+
+local MoveCmd = (function ()
+  ---@class MoveCmd
+  ---@field str string
+  ---@field dx integer
+  ---@field dy integer
+  local MoveCmd = {}
+  MoveCmd.__index = MoveCmd
+
+  ---@param str string
+  function MoveCmd.new(str)
+    local self = setmetatable({}, MoveCmd)
+    self.str = str
+    self.dx = ((str == ">" and 1) or (str == "<" and -1)) or 0
+    self.dy = ((str == "^" and -1) or (str == "v" and 1)) or 0
+    return self
+  end
+
+  return MoveCmd
+end)()
+
+local Warehouse = (function ()
+  ---@class Warehouse
+  ---@field grid string[][]
+  ---@field boxes Box[]
+  ---@field robot Robot
+  local Warehouse = {}
+  Warehouse.__index = Warehouse
+
+  ---@param grid string[][]
+  ---@param boxes Box[]
+  ---@param robot Robot
+  function Warehouse.new(grid, boxes, robot)
+    local self = setmetatable({}, Warehouse)
+    self.grid = grid
+    self.boxes = boxes
+    self.robot = robot
+    return self
+  end
+
+  function Warehouse:print()
+    --[[ copy the grid ]]
+    local grid = {}
+    for y in ipairs(self.grid) do
+      grid[y] = {}
+      for x, v in ipairs(self.grid[y]) do
+        grid[y][x] = v
+      end
+    end
+    --[[ place the boxes ]]
+    for _, box in ipairs(self.boxes) do
+      grid[box.y][box.x] = "O"
+    end
+    --[[ place the robot ]]
+    grid[self.robot.y][self.robot.x] = "@"
+
+    for y in ipairs(grid) do
+      for x in ipairs(grid[y]) do
+        printf(grid[y][x])
+        if x == #grid[y] then
+          printf("\n")
+        end
+      end
+    end
+  end
+
+  ---@param moveCmd MoveCmd
+  function Warehouse:moveBot(moveCmd)
+    
+  end
+
+  return Warehouse
+end)()
+
+local warehouseModule = {
+  Warehouse = Warehouse,
+  Box = Box,
+  Robot = Robot,
+  MoveCmd = MoveCmd,
+}
+
+return warehouseModule
