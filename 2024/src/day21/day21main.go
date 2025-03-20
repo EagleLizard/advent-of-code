91b1aa4c246d1b5b500946ecec9a2163fe6a44c2
eaglelizard <whitedtd@gmail.com>
2025-03-20 10:27:11 -0600

    2024 d21p1 [go] init

--- 2024/src/day21/day21main.go
+++ 2024/src/day21/day21main.go
@@ -1,6 +1,9 @@
 package day21
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 func Day21Part1(inputLines []string) int {
 	day21Input := parseInput(inputLines)
@@ -9,9 +12,21 @@ func Day21Part1(inputLines []string) int {
 	return -1
 }
 
-type Day21Input struct{}
+type Day21Input struct {
+	Keycodes []string
+}
 
 func parseInput(inputLines []string) Day21Input {
-	res := Day21Input{}
+	keycodeRx := regexp.MustCompile(`\d+A`)
+	keycodes := []string{}
+	for _, inputLine := range inputLines {
+		rxMatch := keycodeRx.FindString(inputLine)
+		if len(rxMatch) > 0 {
+			keycodes = append(keycodes, rxMatch)
+		}
+	}
+	res := Day21Input{
+		Keycodes: keycodes,
+	}
 	return res
 }
